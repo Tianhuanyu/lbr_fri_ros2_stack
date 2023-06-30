348a8ddef0e5dffb1a1dbbc42a663f49408d2564
thy <fsfsthy@gmail.com>
2023-06-30 10:52:57 +0100

    1

--- lbr_demos/lbr_fri_ros2_advanced_python_demos/lbr_fri_ros2_advanced_python_demos/admittance_control_Hybrid.py
+++ lbr_demos/lbr_fri_ros2_advanced_python_demos/lbr_fri_ros2_advanced_python_demos/admittance_control_Hybrid.py
@@ -20,7 +20,7 @@ from std_msgs.msg import Float64MultiArray
 
 import pathlib
 import csv
-
+import copy
 # OpTaS
 import optas
 from optas.spatialmath import *
@@ -28,9 +28,338 @@ from optas.spatialmath import *
 from scipy.spatial.transform import Rotation as Rot
 from geometry_msgs.msg import Pose
 
+
 # tau = {0} [-1.1552312   3.70274581 -1.09817682 -3.02117871  0.23070508  0.51952975
 #  -0.39371812]
+def ExtractFromParamsCsv(path):
+        # path_pos = os.path.join(
+        #     get_package_share_directory("gravity_compensation"),
+        #     "test",
+        #     "measurements_with_ext_tau.csv",
+        # )
+        params = []
+        with open(path) as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                # print("111 = {0}".format(row.values()))
+                params = [float(x) for x in list(row.values())]
 
+        return params
+
+def RNEA_function(Nb,Nk,rpys,xyzs,axes):
+    # 2. RNEA
+    Nf = Nb+Nk
+    """
+    input: q, qdot, qddot, model
+    output: tau
+    """
+    
+    om0 = cs.DM([0.0,0.0,0.0])
+    om0D = cs.DM([0.0,0.0,0.0])
+    # gravity_para = cs.DM([0.0, 0.0, -9.81])
+    gravity_para = cs.DM([4.905, 0.0, -8.496])
+
+    """
+    The definination of joint position from joint0 to joint(Nb-1)
+    """
+
+    q = cs.SX.sym('q', Nb, 1)
+    qd = cs.SX.sym('qd', Nb, 1)
+    qdd = cs.SX.sym('qdd', Nb, 1)
+
+    """
+    The definination of mass for link1 to linkee
+    The definination of center of Mass for link1 to linkee
+    The definination of Inertial tensor for link1 to linkee
+    """
+    m = cs.SX.sym('m', 1, Nb+1)
+    cm = cs.SX.sym('cm',3,Nb+1)
+    Icm = cs.SX.sym('Icm',3,3*Nb+3)
+
+    """
+    external force given by link0
+    The list will be appended via RNEA
+    """
+    fs = [cs.DM([0.0,0.0,0.0])]
+    ns = [cs.DM([0.0,0.0,0.0])]
+
+
+    """
+    $$ Forward part of RNEA $$
+    oms,omDs,vDs given by link0. The list will be appended via RNEA.
+    Notes: the gravity_para represents a base acceration to subsitute the gravity.
+
+    """
+    oms = [om0]
+    omDs = [om0D]
+    vDs = [-gravity_para]
+    
+    
+    # 2.1 forward part of RNEA
+    """
+    link0->1, link1->2 ...., link7->end-effector (for example)
+    joint0,   joint1 ....
+    
+    """
+    
+    for i in range(Nf):
+        print("link p({0}) to i{1}".format(i,i+1))
+        if(i!=Nf-1):
+            # print(joints_list_r1)
+            iRp = (rpy2r(rpys[i]) @ angvec2r(q[i], axes[i])).T
+            iaxisi = iRp @ axes[i]
+            omi = iRp @ oms[i] + iaxisi* qd[i]
+            omDi = iRp @ omDs[i] +  skew(iRp @oms[i]) @ (iaxisi*qd[i]) + iaxisi*qdd[i]
+        else:
+            iRp = rpy2r(rpys[i]).T
+            omi = iRp @ oms[i]
+            omDi = iRp @ omDs[i]
+
+        vDi = iRp @ (vDs[i] 
+                        + skew(omDs[i]) @ xyzs[i]
+                    + skew(oms[i]) @ (skew(oms[i])@ xyzs[i]))
+        
+        fi = m[i] * (vDi + skew(omDi)@ cm[:,i]+ skew(omi)@(skew(omi)@cm[:,i]))
+        ni = Icm[:,i*3:i*3+3] @ omDi + skew(omi) @ Icm[:,i*3:i*3+3] @ omi #+ skew(cm[:,i]) @ fi
+        
+
+        oms.append(omi)
+        omDs.append(omDi)
+        vDs.append(vDi)
+        fs.append(fi)
+        ns.append(ni)
+
+
+    """
+    $$ Backward part of RNEA $$
+    """
+
+    # pRi = rpy2r(rpys[-1])
+    ifi = fs[-1]#cs.DM([0.0,0.0,0.0])
+    ini = ns[-1] + skew(cm[:,-1]) @ fs[-1]#cs.DM([0.0,0.0,0.0])
+    # ifi = cs.DM([0.0,0.0,0.0])
+    # ini = cs.DM([0.0,0.0,0.0])
+    taus = []
+
+    # print("Backward: fs[i+1] {0}".format(len(fs)))
+    for i in range(Nf-1,0,-1):
+
+        print("Backward: link i({0}) to p{1}".format(i+1,i))
+        # print("Backward: fs[i+1]".format(fs[i+1]))
+        if(i < Nf-1):
+            pRi = rpy2r(rpys[i]) @ angvec2r(q[i], axes[i])
+        elif(i == Nf-1):
+            pRi = rpy2r(rpys[i])
+        else:
+            pRi = rpy2r(rpys[i])
+        
+
+        ini = ns[i] + pRi @ ini +skew(cm[:,i-1]) @ fs[i] +skew(xyzs[i]) @ pRi @ifi
+        ifi= pRi @ ifi + fs[i]
+        pRi = rpy2r(rpys[i-1]) @ angvec2r(q[i-1], axes[i-1])
+        _tau = ini.T @pRi.T @ axes[i-1]
+        taus.append(_tau)
+
+
+        
+    tau_=cs.vertcat(*[taus[k] for k in range(len(taus)-1,-1,-1)])
+    dynamics_ = optas.Function('dynamics', [q,qd,qdd,m,cm,Icm], [tau_])
+    return dynamics_
+
+
+
+def DynamicLinearlization(dynamics_,Nb):
+    """
+    The definination of joint position from joint0 to joint(Nb-1)
+    """
+
+    q = cs.SX.sym('q', Nb, 1)
+    qd = cs.SX.sym('qd', Nb, 1)
+    qdd = cs.SX.sym('qdd', Nb, 1)
+
+    """
+    The definination of mass for link1 to linkee
+    The definination of center of Mass for link1 to linkee
+    The definination of Inertial tensor for link1 to linkee
+    """
+    m = cs.SX.sym('m', 1, Nb+1)
+    cm = cs.SX.sym('cm',3,Nb+1)
+    Icm = cs.SX.sym('Icm',3,3*Nb+3)
+
+    
+    Y = []
+        
+    for i in range(Nb):
+        # for every link
+        # Y_line = []
+        Y_line = []
+        # PI_a = []
+        for j in range(m.shape[1]):
+            # for every parameters
+            ## 1. get mass
+            m_indu = np.zeros([m.shape[1],m.shape[0]])
+            cm_indu = np.zeros([3,Nb+1])#np.zeros([cm.shape[1],cm.shape[0]])
+            Icm_indu = np.zeros([3,3*Nb+3])#np.zeros([Icm.shape[1],Icm.shape[0]])
+            # print(*m.shape)
+            m_indu[j] = 1.0
+            # print(m_indu)
+
+            output = dynamics_(q,qd,qdd,m_indu,cm_indu,Icm_indu)[i]
+            Y_line.append(output)
+
+
+            ## 2. get cmx
+            output1 = dynamics_(q,qd,qdd,m_indu,cm,Icm_indu)[i]-output
+            for k in range(3):
+                output_cm = optas.jacobian(output1,cm[k,j])
+                output_cm1 = optas.substitute(output_cm,cm,cm_indu)
+                Y_line.append(output_cm1)
+
+            ## 3.get Icm
+            output2 = dynamics_(q,qd,qdd,m_indu,cm_indu,Icm)[i]-output
+            for k in range(3):
+                for l in range(k,3,1):
+                    output_Icm = optas.jacobian(output2,Icm[k,l+3*j])
+                    Y_line.append(output_Icm)
+
+            # sx_lst = optas.horzcat(*Y_seg)
+            # Y_line
+        sx_lst = optas.horzcat(*Y_line)
+        Y.append(sx_lst)
+        # print("Y_line shape = {0}, {1}".format(Y_line[0].shape[0],Y_line[0].shape[1]))
+        # print("sx_lst shape = {0}, {1}".format(sx_lst.shape[0],sx_lst.shape[1]))
+
+    Y_mat = optas.vertcat(*Y)
+    # print(Y_mat)
+    print("Y_mat shape = {0}, {1}".format(Y_mat.shape[0],Y_mat.shape[1]))
+    Ymat = optas.Function('Dynamic_Ymat',[q,qd,qdd],[Y_mat])
+
+    PI_a = []
+    for j in range(m.shape[1]):
+        # for every parameters
+        pi_temp = [m[j],
+                    m[j]*cm[0,j],
+                    m[j]*cm[1,j],
+                    m[j]*cm[2,j],
+                    Icm[0,0+3*j] + m[j]*(cm[1,j]*cm[1,j]+cm[2,j]*cm[2,j]),  # XXi
+                    Icm[0,1+3*j] - m[j]*(cm[0,j]*cm[1,j]),  # XYi
+                    Icm[0,2+3*j] - m[j]*(cm[0,j]*cm[2,j]),  # XZi
+                    Icm[1,1+3*j] + m[j]*(cm[0,j]*cm[0,j]+cm[2,j]*cm[2,j]),  # YYi
+                    Icm[1,2+3*j] - m[j]*(cm[1,j]*cm[2,j]),  # YZi
+                    Icm[2,2+3*j] + m[j]*(cm[0,j]*cm[0,j]+cm[1,j]*cm[1,j])] # ZZi
+        PI_a.append(optas.vertcat(*pi_temp))
+
+    PI_vecter = optas.vertcat(*PI_a)
+    print("PI_vecter shape = {0}, {1}".format(PI_vecter.shape[0],PI_vecter.shape[1]))
+    PIvector = optas.Function('Dynamic_PIvector',[m,cm,Icm],[PI_vecter])
+
+    return Ymat, PIvector
+
+def find_eigen_value(dof, parm_num, regressor_func,shape):
+    '''
+    Find dynamic parameter dependencies (i.e., regressor column dependencies).
+    '''
+
+    samples = 100
+    round = 10
+
+    pi = np.pi
+
+    # Z = np.zeros((dof * samples, parm_num))
+    A_mat = np.zeros(( shape,shape ))
+
+    for i in range(samples):
+        a = np.random.random([parm_num,dof])*2.0-1.0
+        b = np.random.random([parm_num,dof])*2.0-1.0
+
+        A_mat = A_mat+ regressor_func(a,b)
+
+    print("U V finished")
+    U, s, V = np.linalg.svd(A_mat)
+        # Z[i * dof: i * dof + dof, :] = np.matrix(
+        #     regressor_func(q, dq, ddq)).reshape(dof, parm_num)
+
+    # R1_diag = np.linalg.qr(Z, mode='r').diagonal().round(round)
+    
+
+    return U,V
+
+def getJointParametersfromURDF(robot, ee_link="lbr_link_ee"):
+    robot_urdf = robot.urdf
+    root = robot_urdf.get_root()
+    # ee_link = "lbr_link_ee"
+    xyzs, rpys, axes = [], [], []
+
+
+    joints_list = robot_urdf.get_chain(root, ee_link, links=False)
+    # print("joints_list = {0}"
+    #         .format(joints_list)
+    #         )
+    # assumption: The first joint is fixed. The information in this joint is not recorded
+    """
+    xyzs starts from joint 0 to joint ee
+    rpys starts from joint 0 to joint ee
+    axes starts from joint 0 to joint ee
+    """
+    joints_list_r = joints_list[1:]
+    for joint_name in joints_list_r:
+        print(joint_name)
+        joint = robot_urdf.joint_map[joint_name]
+        xyz, rpy = robot.get_joint_origin(joint)
+        axis = robot.get_joint_axis(joint)
+
+        # record the kinematic parameters
+        xyzs.append(xyz)
+        rpys.append(rpy)
+        axes.append(axis)
+    print("xyz, rpy, axis = {0}, {1} ,{2}".format(xyzs, rpys, axes))
+
+    Nb = len(joints_list_r)-1
+    return Nb, xyzs, rpys, axes
+
+
+
+def find_dyn_parm_deps(dof, parm_num, regressor_func):
+    '''
+    Find dynamic parameter dependencies (i.e., regressor column dependencies).
+    '''
+
+    samples = 10000
+    round = 10
+
+    pi = np.pi
+
+    Z = np.zeros((dof * samples, parm_num))
+
+    for i in range(samples):
+        q = [float(np.random.random() * 2.0 * pi - pi) for j in range(dof)]
+        dq = [float(np.random.random() * 2.0 * pi - pi) for j in range(dof)]
+        ddq = [float(np.random.random() * 2.0 * pi - pi)
+            for j in range(dof)]
+        Z[i * dof: i * dof + dof, :] = np.matrix(
+            regressor_func(q, dq, ddq)).reshape(dof, parm_num)
+
+    R1_diag = np.linalg.qr(Z, mode='r').diagonal().round(round)
+    dbi = []
+    ddi = []
+    for i, e in enumerate(R1_diag):
+        if e != 0:
+            dbi.append(i)
+        else:
+            ddi.append(i)
+    dbn = len(dbi)
+
+    P = np.mat(np.eye(parm_num))[:, dbi + ddi]
+    Pb = P[:, :dbn]
+    Pd = P[:, dbn:]
+
+    Rbd1 = np.mat(np.linalg.qr(Z * P, mode='r'))
+    Rb1 = Rbd1[:dbn, :dbn]
+    Rd1 = Rbd1[:dbn, dbn:]
+
+    Kd = np.mat((np.linalg.inv(Rb1) * Rd1).round(round))
+
+    return Pb, Pd, Kd
 
 # ros2 launch lbr_bringup lbr_bringup.launch.py model:=med7 sim:=false controller_configurations_package:=lbr_velocity_controllers controller_configurations:=config/sample_config.yml controller:=lbr_velocity_controller 
 def csv_save(path, vector):
@@ -200,7 +529,7 @@ class TrackingController:
         
         diffR = Rg_ee.T @ R
         cvf = Rc.T @ fh[:3]
-        diffFl =optas.diag([0.02, 0.02, 0.02]) @ Rc.T @ fh[:3] -  Rc.T @ dp[:3]
+        diffFl =optas.diag([0.25, 0.25, 0.25]) @ Rc.T @ fh[:3] -  Rc.T @ dp[:3]
         
 
 
@@ -229,25 +558,25 @@ class TrackingController:
 
  
         builder.add_leq_inequality_constraint(
-            "dq1", dq[0] * dq[0], 4e1
+            "dq1", dq[0] * dq[0], 4e2
         )
         builder.add_leq_inequality_constraint(
-            "dq2", dq[1] * dq[1], 4e1
+            "dq2", dq[1] * dq[1], 4e2
         )
         builder.add_leq_inequality_constraint(
-            "dq3", dq[2] * dq[2], 4e1
+            "dq3", dq[2] * dq[2], 4e2
         )
         builder.add_leq_inequality_constraint(
-            "dq4", dq[3] * dq[3], 4e1
+            "dq4", dq[3] * dq[3], 4e2
         )
         builder.add_leq_inequality_constraint(
-            "dq5", dq[4] * dq[4], 4e1
+            "dq5", dq[4] * dq[4], 4e2
         )
         builder.add_leq_inequality_constraint(
-            "dq6", dq[5] * dq[5], 4e1
+            "dq6", dq[5] * dq[5], 4e2
         )
         builder.add_leq_inequality_constraint(
-            "dq7", dq[6] * dq[6], 4e1
+            "dq7", dq[6] * dq[6], 4e2
         )
         # builder.add_leq_inequality_constraint(
         #     "eff_z", diffp[2] * diffp[2], 1e-8
@@ -346,17 +675,29 @@ class AdmittanceControlNode(Node):
             data=self.urdf_string_, end_link_name=self.end_link_name_, root_link_name=self.root_link_name_
         )
 
-        # self.chain_ee = kinpy.build_serial_chain_from_urdf(
-        #     data=self.urdf_string_, end_link_name=self.end_link_name_, root_link_name=self.root_link_name_
-        # )
 
+        self.robot = optas.RobotModel(
+            xacro_filename=path,
+            time_derivs=[1],  # i.e. joint velocity
+        )
+        Nb, xyzs, rpys, axes = getJointParametersfromURDF(self.robot)
+        self.dynamics_ = RNEA_function(Nb,1,rpys,xyzs,axes)
+        self.Ymat, self.PIvector = DynamicLinearlization(self.dynamics_,Nb)
 
-        # The controller based on conventional controller
-        # self.controller_ = Controller(
-        #     urdf_string=path,
-        #     end_link_name=self.end_link_name_,
-        #     root_link_name=self.root_link_name_,
-        # )
+        Pb, Pd, Kd =find_dyn_parm_deps(7,80,self.Ymat)
+        K = Pb.T +Kd @Pd.T
+
+        self.Pb = Pb
+        self.K = K
+
+        path_pos = os.path.join(
+            get_package_share_directory("gravity_compensation"),
+            "test",
+            "DynamicParameters.csv",
+        )
+        self.params = ExtractFromParamsCsv(path_pos)
+
+        print("self.params = {0}".format(self.params))
 
         # The controller based on optas
         self.controller_optas = TrackingController(
@@ -396,7 +737,10 @@ class AdmittanceControlNode(Node):
 
         self.init_ = True
         self.command_init_ = False
-        self.goal_pose_ = None        
+        self.goal_pose_ = None
+        self.qd = np.array([0.0]*7)
+        self.qd_last = np.array([0.0]*7)
+        self.pa_size = Pb.shape[1]
 
         # pg = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
@@ -404,12 +748,13 @@ class AdmittanceControlNode(Node):
         if not self.lbr_state_:
             return
         q = np.array(self.lbr_state_.measured_joint_position.tolist())
-        tau = np.array(self.lbr_state_.external_torque.tolist())
+        tau = np.array(self.lbr_state_.measured_torque.tolist())
 
         if(self.init_):
             self.pg = self.chain_.forward_kinematics(q)
             print(self.pg)
             self.init_ = False
+            self.q_last = copy.deepcopy(q)
             return
         else:
             pass
@@ -424,19 +769,32 @@ class AdmittanceControlNode(Node):
 
 
         # dq, f_ext = self.controller_(q, tau_ext)
+        qdd = (self.qd-self.qd_last)/0.01
+
+        tau_ext = tau - (self.Ymat(q.tolist(),
+                                   self.qd.tolist(),
+                                   qdd.tolist())@self.Pb @  self.params[:self.pa_size] + 
+                np.diag(np.sign(self.qd)) @ self.params[self.pa_size:self.pa_size+7]+ 
+                np.diag(qdd) @ self.params[self.pa_size+7:])
 
         # TODO
         # Target position should be given according to vision. A static one can be given firstly
         # Param: pg ->
-        dq, f_ext = self.controller_optas(q, pg ,tau)
+        print("tau_ext = {0}".format(tau_ext))
+        # print("tau = {0}".format(tau))
+        # print("self.qd = {0}".format(self.qd))
+        # print("tau_ext = {0}".format(tau_ext.toarray().flatten().shape))
+        # print("tau = {0}".format(tau.shape))
+        dq, f_ext = self.controller_optas(q, pg ,tau_ext.toarray().flatten())
         # TrackingController()
-
+        self.qd_last = copy.deepcopy(self.qd)
+        self.qd = q-self.q_last
+        self.q_last = copy.deepcopy(q)
         # command
         self.lbr_command_.joint_position = (
             np.array(self.lbr_state_.measured_joint_position.tolist())
-            + self.lbr_state_.sample_time * dq * 4.0
+            + self.lbr_state_.sample_time * dq * 1.0
         ).data
-        print("tau = {0}",format(tau))
         # print("self.lbr_state_.sample_time * dq * 1.0 = {0}",format(self.lbr_state_.sample_time * dq * 1.0))
         # print("goal = {0}",format(self.goal_pose_))
         if(self.goal_pose_ is not None):
